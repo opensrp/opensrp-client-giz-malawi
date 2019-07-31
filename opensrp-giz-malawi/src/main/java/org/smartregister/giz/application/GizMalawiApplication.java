ff277b1498c24a13a212bd80872e95051bdc08ae
Ephraim Kigamba <ekigamba@ona.io>
2019-07-31 18:46:47 +0300

    Fix crashes, ec_mother FTS fix

--- opensrp-giz-malawi/src/main/java/org/smartregister/giz/application/GizMalawiApplication.java
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/application/GizMalawiApplication.java
@@ -15,6 +15,7 @@ import org.jetbrains.annotations.NotNull;
 import org.smartregister.Context;
 import org.smartregister.CoreLibrary;
 import org.smartregister.anc.library.AncLibrary;
+import org.smartregister.anc.library.util.DBConstants;
 import org.smartregister.child.ChildLibrary;
 import org.smartregister.child.activity.BaseChildFormActivity;
 import org.smartregister.child.domain.ChildMetadata;
@@ -92,13 +93,16 @@ public class GizMalawiApplication extends DrishtiApplication implements TimeChan
     }
 
     private static String[] getFtsTables() {
-        return new String[]{GizConstants.TABLE_NAME.CHILD};
+        return new String[]{GizConstants.TABLE_NAME.CHILD, DBConstants.WOMAN_TABLE_NAME};
     }
 
     private static String[] getFtsSearchFields(String tableName) {
         if (tableName.equals(GizConstants.TABLE_NAME.CHILD)) {
             return new String[]{GizConstants.KEY.ZEIR_ID, GizConstants.KEY.FIRST_NAME, GizConstants.KEY.LAST_NAME};
+        } else if (tableName.equalsIgnoreCase(DBConstants.WOMAN_TABLE_NAME)) {
+            return new String[]{DBConstants.KEY.FIRST_NAME, DBConstants.KEY.LAST_NAME, DBConstants.KEY.ANC_ID};
         }
+
         return null;
     }
 
@@ -121,7 +125,11 @@ public class GizMalawiApplication extends DrishtiApplication implements TimeChan
             }
 
             return names.toArray(new String[names.size()]);
+        } else if (tableName.equals(DBConstants.WOMAN_TABLE_NAME)) {
+            return new String[]{DBConstants.KEY.BASE_ENTITY_ID, DBConstants.KEY.FIRST_NAME, DBConstants.KEY.LAST_NAME,
+                    DBConstants.KEY.LAST_INTERACTED_WITH, DBConstants.KEY.DATE_REMOVED};
         }
+
         return null;
     }
 
