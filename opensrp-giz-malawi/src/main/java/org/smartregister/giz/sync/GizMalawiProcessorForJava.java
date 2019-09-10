97a7ebe1fbb08beff420cdb7859764325035eb04
bennsimon <botieno@ona.io>
2019-09-10 15:57:00 +0300

    removed unused imports

--- opensrp-giz-malawi/src/main/java/org/smartregister/giz/sync/GizMalawiProcessorForJava.java
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/sync/GizMalawiProcessorForJava.java
@@ -9,7 +9,6 @@ import org.apache.commons.lang3.math.NumberUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.joda.time.DateTime;
-import org.smartregister.child.ChildLibrary;
 import org.smartregister.child.util.Constants;
 import org.smartregister.child.util.JsonFormUtils;
 import org.smartregister.child.util.MoveToMyCatchmentUtils;
