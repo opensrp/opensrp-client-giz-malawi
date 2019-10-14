6f7681717ccb47b1d928ca6347dcca27d3967143
Ephraim Kigamba <ekigamba@ona.io>
2019-10-14 16:21:16 +0300

    Move inject form intent metadata functionality back to opd

--- opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/OpdFormActivity.java
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/OpdFormActivity.java
@@ -15,29 +15,6 @@ import java.util.Set;
 
 public class OpdFormActivity extends BaseOpdFormActivity {
 
-    private HashMap<String, String> parcelableData = new HashMap<>();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        Bundle extras = getIntent().getExtras();
-        if (getIntent() != null && extras != null) {
-            Set<String> keySet = extras.keySet();
-
-            for (String key: keySet) {
-                if (!key.equals(OpdConstants.JSON_FORM_EXTRA.JSON)) {
-                    Object objectValue = extras.get(key);
-
-                    if (objectValue instanceof String) {
-                        String value = (String) objectValue;
-                        parcelableData.put(key, value);
-                    }
-                }
-            }
-        }
-    }
-
     @Override
     public void initializeFormFragment() {
         initializeFormFragmentCore();
@@ -46,10 +23,5 @@ public class OpdFormActivity extends BaseOpdFormActivity {
     protected void initializeFormFragmentCore() {
         OpdFormFragment opdFormFragment = OpdFormFragment.getFormFragment(JsonFormConstants.FIRST_STEP_NAME);
         getSupportFragmentManager().beginTransaction().add(com.vijay.jsonwizard.R.id.container, opdFormFragment).commit();
-    }
-
-    @NonNull
-    public HashMap<String, String> getParcelableData() {
-        return parcelableData;
     }
 }
