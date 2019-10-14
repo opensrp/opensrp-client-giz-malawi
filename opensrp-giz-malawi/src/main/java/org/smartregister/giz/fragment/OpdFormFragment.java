bd40c976a9e5d2221473e3c392f6690e22619f87
Ephraim Kigamba <ekigamba@ona.io>
2019-10-14 16:21:16 +0300

    Move inject form intent metadata functionality back to opd

--- opensrp-giz-malawi/src/main/java/org/smartregister/giz/fragment/OpdFormFragment.java
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/fragment/OpdFormFragment.java
@@ -1,20 +1,14 @@
 package org.smartregister.giz.fragment;
 
-
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.vijay.jsonwizard.constants.JsonFormConstants;
 import com.vijay.jsonwizard.viewstates.JsonFormFragmentViewState;
 
-import org.smartregister.giz.activity.OpdFormActivity;
 import org.smartregister.opd.adapter.ClientLookUpListAdapter;
 import org.smartregister.opd.fragment.BaseOpdFormFragment;
 import org.smartregister.opd.interactor.OpdFormInteractor;
 import org.smartregister.opd.presenter.OpdFormFragmentPresenter;
-
-import java.util.HashMap;
 
 
 public class OpdFormFragment extends BaseOpdFormFragment implements ClientLookUpListAdapter.ClickListener {
@@ -36,27 +30,5 @@ public class OpdFormFragment extends BaseOpdFormFragment implements ClientLookUp
         bundle.putString(JsonFormConstants.JSON_FORM_KEY.STEPNAME, stepName);
         jsonFormFragment.setArguments(bundle);
         return jsonFormFragment;
-    }
-
-    @Override
-    public void finishWithResult(Intent returnIntent) {
-        Activity activity = getActivity();
-
-        if (activity instanceof OpdFormActivity) {
-            OpdFormActivity opdFormActivity = (OpdFormActivity) activity;
-
-            HashMap<String, String> parcelableData = opdFormActivity.getParcelableData();
-
-            for (String key: parcelableData.keySet()) {
-                String value = parcelableData.get(key);
-
-                if (value != null) {
-                    returnIntent.putExtra(key, value);
-                }
-            }
-        }
-
-        getActivity().setResult(Activity.RESULT_OK, returnIntent);
-        getActivity().finish();
     }
 }
