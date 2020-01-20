d08da462a329ec929babe27288ffd257337e34e7
Ephraim Kigamba <ekigamba@ona.io>
2020-01-20 10:46:54 +0300

    Code cleanup & fix tests

--- opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/GizJsonFormActivity.java
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/GizJsonFormActivity.java
@@ -1,83 +1,24 @@
 package org.smartregister.giz.activity;
 
-import android.os.Bundle;
-
+import com.vijay.jsonwizard.activities.JsonWizardFormActivity;
 import com.vijay.jsonwizard.constants.JsonFormConstants;
 
-import org.apache.commons.lang3.StringUtils;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.smartregister.giz.fragment.GizMalawiJsonFormFragment;
-import org.smartregister.stock.activity.StockJsonFormActivity;
 
 /**
  * Created by Ephraim Kigamba - ekigamba@ona.io on 2019-07-11
  */
 
-public class GizJsonFormActivity extends StockJsonFormActivity {
+public class GizJsonFormActivity extends JsonWizardFormActivity {
 
     private GizMalawiJsonFormFragment wellnessJsonFormFragment;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     public void initializeFormFragment() {
         wellnessJsonFormFragment = GizMalawiJsonFormFragment.getFormFragment(JsonFormConstants.FIRST_STEP_NAME);
         getSupportFragmentManager().beginTransaction()
                 .add(com.vijay.jsonwizard.R.id.container, wellnessJsonFormFragment).commit();
-    }
-
-    @Override
-    public void writeValue(String stepName, String key, String value, String openMrsEntityParent, String openMrsEntity, String openMrsEntityId) throws JSONException {
-        super.writeValue(stepName, key, value, openMrsEntityParent, openMrsEntity, openMrsEntityId);
-        refreshCalculateLogic(key, value);
-    }
-
-    public boolean checkIfBalanceNegative() {
-        boolean balancecheck = true;
-        String balancestring = wellnessJsonFormFragment.getRelevantTextViewString("Balance");
-
-        if (balancestring.contains("New balance") && StringUtils.isNumeric(balancestring)) {
-            int balance = Integer.parseInt(balancestring.replace("New balance:", "").trim());
-            if (balance < 0) {
-                balancecheck = false;
-            }
-        }
-
-        return balancecheck;
-    }
-
-    public boolean checkIfAtLeastOneServiceGiven() {
-        JSONObject object = getStep("step1");
-        try {
-            if (object.getString("title").contains("Record out of catchment area service")) {
-                JSONArray fields = object.getJSONArray("fields");
-                for (int i = 0; i < fields.length(); i++) {
-                    JSONObject vaccineGroup = fields.getJSONObject(i);
-                    if (vaccineGroup.has("key") && vaccineGroup.has("is_vaccine_group")) {
-                        if (vaccineGroup.getBoolean("is_vaccine_group") && vaccineGroup.has("options")) {
-                            JSONArray vaccineOptions = vaccineGroup.getJSONArray("options");
-                            for (int j = 0; j < vaccineOptions.length(); j++) {
-                                JSONObject vaccineOption = vaccineOptions.getJSONObject(j);
-                                if (vaccineOption.has("value") && vaccineOption.getBoolean("value")) {
-                                    return true;
-                                }
-                            }
-                        }
-                    } else if (vaccineGroup.has("key") && vaccineGroup.getString("key").equals("Weight_Kg") && vaccineGroup.has("value") && vaccineGroup.getString("value").length() > 0) {
-                        return true;
-                    }
-                }
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        return false;
     }
 
 }
