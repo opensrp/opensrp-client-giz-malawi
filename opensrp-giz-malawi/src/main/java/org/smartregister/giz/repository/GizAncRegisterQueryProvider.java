72f742efdac6e29c8d5cbb8331abc91475f3be15
bennsimon <botieno@ona.io>
2020-03-18 15:15:41 +0300

    code clean up

--- opensrp-giz-malawi/src/main/java/org/smartregister/giz/repository/GizAncRegisterQueryProvider.java
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/repository/GizAncRegisterQueryProvider.java
@@ -4,40 +4,43 @@ import org.apache.commons.lang3.StringUtils;
 import org.smartregister.anc.library.repository.RegisterQueryProvider;
 import org.smartregister.anc.library.util.ConstantsUtils;
 import org.smartregister.anc.library.util.DBConstantsUtils;
-import org.smartregister.commonregistry.CommonFtsObject;
 import org.smartregister.giz.util.GizConstants;
 
 public class GizAncRegisterQueryProvider extends RegisterQueryProvider {
 
     @Override
-    public String getObjectIdsQuery(String mainCondition, String filters) {
-        String strMainCondition = getMainCondition(mainCondition);
+    public String getObjectIdsQuery(String tempMainCondition, String tempFilters) {
+        String mainCondition = tempMainCondition;
+        String filters = tempFilters;
+        if (!filters.isEmpty()) {
+            filters = String.format(" and ec_client_search.phrase MATCH '*%s*'", filters);
+        }
 
-        String strFilters = getFilter(filters);
-
-        if (StringUtils.isNotBlank(strFilters) && StringUtils.isBlank(strMainCondition)) {
-            strFilters = String.format(" where " + getDemographicTable() + "." + CommonFtsObject.phraseColumn + " MATCH '*%s*'", filters);
+        if (!StringUtils.isBlank(mainCondition)) {
+            mainCondition = " and " + mainCondition;
         }
 
         return "select ec_client_search.object_id from ec_client_search " +
                 "join ec_mother_details on ec_client_search.object_id =  ec_mother_details.id " +
                 "join client_register_type on ec_client_search.object_id=client_register_type.base_entity_id " +
-                "where register_type='anc' " + strMainCondition + strFilters;
+                "where register_type='anc' " + mainCondition + filters;
     }
 
-    public String getCountExecuteQuery(String mainCondition, String filters) {
-        String strFilters = getFilter(filters);
-
-        if (StringUtils.isNotBlank(filters) && StringUtils.isBlank(mainCondition)) {
-            strFilters = String.format(" where " + CommonFtsObject.searchTableName(getDemographicTable()) + "." + CommonFtsObject.phraseColumn + " MATCH '*%s*'", filters);
+    public String getCountExecuteQuery(String tempMainCondition, String tempFilters) {
+        String filters = tempFilters;
+        String mainCondition = tempMainCondition;
+        if (!filters.isEmpty()) {
+            filters = String.format(" and ec_client_search.phrase MATCH '*%s*'", filters);
         }
 
-        String strMainCondition = getMainCondition(mainCondition);
+        if (!StringUtils.isBlank(mainCondition)) {
+            mainCondition = " and " + mainCondition;
+        }
 
         return "select count(ec_client_search.object_id) from ec_client_search " +
                 "join ec_mother_details on ec_client_search.object_id =  ec_mother_details.id " +
                 "join client_register_type on ec_client_search.object_id=client_register_type.base_entity_id " +
-                "where register_type='anc' " + strMainCondition + strFilters;
+                "where register_type='anc' " + mainCondition + filters;
     }
 
 
