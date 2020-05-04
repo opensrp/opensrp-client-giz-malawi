ca2452d90a26389eda867d8c2238fe5d46e79a4e
bennsimon <botieno@ona.io>
2020-05-04 12:33:33 +0300

    update code with martin's fixes

--- opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/ChildImmunizationActivity.java
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/ChildImmunizationActivity.java
@@ -73,6 +73,7 @@ public class ChildImmunizationActivity extends BaseChildImmunizationActivity {
         bundle.putString(Constants.INTENT_KEY.LOCATION_ID,
                 Utils.context().allSharedPreferences().getPreference(AllConstants.CURRENT_LOCATION_ID));
         bundle.putSerializable(Constants.INTENT_KEY.EXTRA_CHILD_DETAILS, childDetails);
+        bundle.putSerializable(Constants.INTENT_KEY.BASE_ENTITY_ID, childDetails.getCaseId());
         bundle.putSerializable(Constants.INTENT_KEY.EXTRA_REGISTER_CLICKABLES, registerClickables);
         intent.putExtras(bundle);
 
