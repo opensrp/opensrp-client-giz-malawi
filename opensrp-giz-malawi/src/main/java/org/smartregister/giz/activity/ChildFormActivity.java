bf91ba62d9387545e514e0bca679905328315bfe
Martin Ndegwa <ndegwamartin@gmail.com>
2020-05-04 20:03:52 +0300

    Migrate to latest child library - Migrate to child lib version with mother lookup functionality

--- opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/ChildFormActivity.java
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/ChildFormActivity.java
@@ -2,8 +2,14 @@ package org.smartregister.giz.activity;
 
 import com.vijay.jsonwizard.constants.JsonFormConstants;
 
+import org.smartregister.AllConstants;
 import org.smartregister.child.activity.BaseChildFormActivity;
+import org.smartregister.child.util.Constants;
+import org.smartregister.child.util.MotherLookUpUtils;
+import org.smartregister.cursoradapter.SmartRegisterQueryBuilder;
 import org.smartregister.giz.fragment.GizChildFormFragment;
+
+import java.util.Map;
 
 public class ChildFormActivity extends BaseChildFormActivity {
     @Override
@@ -16,4 +22,17 @@ public class ChildFormActivity extends BaseChildFormActivity {
         getSupportFragmentManager().beginTransaction().add(com.vijay.jsonwizard.R.id.container, gizChildFormFragment).commit();
     }
 
+    @Override
+    public String lookUpQuery(Map<String, String> entityMap, String tableName) {
+
+        SmartRegisterQueryBuilder queryBUilder = new SmartRegisterQueryBuilder();
+        queryBUilder.SelectInitiateMainTable(tableName,
+                new String[]{MotherLookUpUtils.RELATIONALID, MotherLookUpUtils.DETAILS, Constants.KEY.ZEIR_ID, Constants.KEY.FIRST_NAME, Constants.KEY.LAST_NAME,
+                        AllConstants.ChildRegistrationFields.GENDER, Constants.KEY.DOB, MotherLookUpUtils.NRC_NUMBER, MotherLookUpUtils.CONTACT_PHONE_NUMBER,
+                        Constants.KEY.BASE_ENTITY_ID}
+
+        );
+        String query = queryBUilder.mainCondition(getMainConditionString(entityMap));
+        return queryBUilder.Endquery(query);
+    }
 }
