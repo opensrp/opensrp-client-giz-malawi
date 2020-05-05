f382217ada052ff9def80c6df2249fee7574d4b8
bennsimon <botieno@ona.io>
2020-05-05 12:43:45 +0300

    update mother match lookup

--- opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/ChildFormActivity.java
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/ChildFormActivity.java
@@ -3,7 +3,12 @@ package org.smartregister.giz.activity;
 import com.vijay.jsonwizard.constants.JsonFormConstants;
 
 import org.smartregister.child.activity.BaseChildFormActivity;
+import org.smartregister.child.util.Constants;
+import org.smartregister.child.util.MotherLookUpUtils;
+import org.smartregister.cursoradapter.SmartRegisterQueryBuilder;
 import org.smartregister.giz.fragment.GizChildFormFragment;
+
+import java.util.Map;
 
 public class ChildFormActivity extends BaseChildFormActivity {
     @Override
@@ -16,4 +21,16 @@ public class ChildFormActivity extends BaseChildFormActivity {
         getSupportFragmentManager().beginTransaction().add(com.vijay.jsonwizard.R.id.container, gizChildFormFragment).commit();
     }
 
+    @Override
+    public String lookUpQuery(Map<String, String> entityMap, String tableName) {
+
+        SmartRegisterQueryBuilder queryBUilder = new SmartRegisterQueryBuilder();
+        queryBUilder.SelectInitiateMainTable(tableName,
+                new String[]{MotherLookUpUtils.RELATIONALID, MotherLookUpUtils.DETAILS, Constants.KEY.ZEIR_ID,
+                        Constants.KEY.FIRST_NAME, Constants.KEY.LAST_NAME,Constants.KEY.DOB, Constants.KEY.BASE_ENTITY_ID}
+
+        );
+        String query = queryBUilder.mainCondition(getMainConditionString(entityMap));
+        return queryBUilder.Endquery(query);
+    }
 }
