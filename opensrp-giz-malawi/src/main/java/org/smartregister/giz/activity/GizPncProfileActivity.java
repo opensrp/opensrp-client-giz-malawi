d312fb5afb12df1314bd34765968927ef5063390
bennsimon <botieno@ona.io>
2020-07-28 18:46:05 +0300

    add repeating group prepopulations to pnc profile

---
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/GizPncProfileActivity.java
@@ -0,0 +1,91 @@
+package org.smartregister.giz.activity;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.smartregister.child.ChildLibrary;
+import org.smartregister.child.util.Constants;
+import org.smartregister.giz.configuration.ChildStatusRepeatingGroupGenerator;
+import org.smartregister.giz.repository.GizChildRegisterQueryProvider;
+import org.smartregister.pnc.activity.BasePncProfileActivity;
+import org.smartregister.pnc.config.RepeatingGroupGenerator;
+import org.smartregister.pnc.utils.PncConstants;
+import org.smartregister.pnc.utils.PncDbConstants;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import timber.log.Timber;
+
+public class GizPncProfileActivity extends BasePncProfileActivity {
+
+    @Override
+    public void startFormActivityFromFormJson(@NonNull String entityId, @NonNull JSONObject jsonForm, @Nullable HashMap<String, String> intentData) {
+        generateRepeatingGrpFields(jsonForm, entityId);
+        super.startFormActivityFromFormJson(entityId, jsonForm, intentData);
+    }
+
+    public void generateRepeatingGrpFields(JSONObject json, String entityId) {
+        if (PncConstants.EventTypeConstants.PNC_OUTCOME.equals(json.optString(PncConstants.JsonFormKeyConstants.ENCOUNTER_TYPE))) {
+            try {
+                RepeatingGroupGenerator repeatingGroupGenerator = new RepeatingGroupGenerator(json.optJSONObject("step4"),
+                        "baby_alive_group",
+                        outcomeColumnMap(),
+                        PncDbConstants.KEY.BASE_ENTITY_ID,
+                        storedValues(entityId));
+                repeatingGroupGenerator
+                        .setFieldsWithoutSpecialViewValidation
+                                (new HashSet<>(
+                                        Arrays.asList("birth_weight_entered", "birth_height_entered", "birth_record_date", "baby_gender", "baby_first_name", "baby_last_name", "baby_dob")));
+                repeatingGroupGenerator.init();
+            } catch (JSONException e) {
+                Timber.e(e);
+            }
+        } else if (PncConstants.EventTypeConstants.PNC_VISIT.equals(json.optString(PncConstants.JsonFormKeyConstants.ENCOUNTER_TYPE))) {
+            try {
+                new ChildStatusRepeatingGroupGenerator(json.optJSONObject("step3"),
+                        "child_status",
+                        visitColumnMap(),
+                        PncDbConstants.KEY.BASE_ENTITY_ID,
+                        storedValues(entityId)).init();
+            } catch (JSONException e) {
+                Timber.e(e);
+            }
+        }
+    }
+
+    @NonNull
+    public ArrayList<HashMap<String, String>> storedValues(String entityId) {
+        GizChildRegisterQueryProvider childRegisterQueryProvider = new GizChildRegisterQueryProvider();
+        return ChildLibrary.
+                getInstance()
+                .context()
+                .getEventClientRepository()
+                .rawQuery(ChildLibrary.getInstance().getRepository().getReadableDatabase(),
+                        childRegisterQueryProvider.mainRegisterQuery() +
+                                " where " + childRegisterQueryProvider.getChildDetailsTable() + "." + Constants.KEY.RELATIONAL_ID + " = '" + entityId + "'");
+    }
+
+    @NonNull
+    public Map<String, String> outcomeColumnMap() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("baby_first_name", "first_name");
+        map.put("baby_last_name", "last_name");
+        map.put("baby_dob", "dob");
+        map.put("baby_gender", "gender");
+        return map;
+    }
+
+    @NonNull
+    public Map<String, String> visitColumnMap() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("child_name", "first_name");
+        map.put("open_vaccine_card", "base_entity_id");
+        return map;
+    }
+}
