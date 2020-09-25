4e601f7b03b4cb394801f2ed657bd3caddff02ff
bennsimon <botieno@ona.io>
2020-09-25 18:04:25 +0300

    add maternityOutcomeProcessingTest

--- opensrp-giz-malawi/src/main/java/org/smartregister/giz/configuration/GizMaternityOutcomeFormProcessing.java
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/configuration/GizMaternityOutcomeFormProcessing.java
@@ -16,7 +16,6 @@ import org.smartregister.child.interactor.ChildRegisterInteractor;
 import org.smartregister.clientandeventmodel.Client;
 import org.smartregister.clientandeventmodel.Event;
 import org.smartregister.domain.tag.FormTag;
-import org.smartregister.giz.util.GizJsonFormUtils;
 import org.smartregister.giz.util.GizUtils;
 import org.smartregister.maternity.MaternityLibrary;
 import org.smartregister.maternity.configuration.MaternityOutcomeFormProcessingTask;
@@ -154,12 +153,12 @@ public class GizMaternityOutcomeFormProcessing extends MaternityOutcomeFormProce
 
         Event maternityOutcomeEvent = MaternityJsonFormUtils.createEvent(fieldsArray, jsonFormObject.getJSONObject(METADATA)
                 , formTag, baseEntityId, MaternityConstants.EventType.MATERNITY_OUTCOME, "");
-        GizJsonFormUtils.tagEventSyncMetadata(maternityOutcomeEvent);
+        MaternityJsonFormUtils.tagSyncMetadata(maternityOutcomeEvent);
         eventList.add(maternityOutcomeEvent);
 
         Event closeMaternityEvent = JsonFormUtils.createEvent(new JSONArray(), new JSONObject(),
                 formTag, baseEntityId, MaternityConstants.EventType.MATERNITY_CLOSE, "");
-        GizJsonFormUtils.tagEventSyncMetadata(closeMaternityEvent);
+        MaternityJsonFormUtils.tagSyncMetadata(closeMaternityEvent);
         closeMaternityEvent.addDetails(MaternityConstants.JSON_FORM_KEY.VISIT_END_DATE, MaternityUtils.convertDate(new Date(), MaternityConstants.DateFormat.YYYY_MM_DD_HH_MM_SS));
         eventList.add(closeMaternityEvent);
 
