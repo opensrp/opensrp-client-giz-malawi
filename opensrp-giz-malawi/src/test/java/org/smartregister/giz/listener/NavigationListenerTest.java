5e1aaa85d81785211f4a871d6c995d23f61632df
bennsimon <botieno@ona.io>
2020-11-04 10:00:02 +0300

    fix codacy

--- opensrp-giz-malawi/src/test/java/org/smartregister/giz/listener/NavigationListenerTest.java
+++ opensrp-giz-malawi/src/test/java/org/smartregister/giz/listener/NavigationListenerTest.java
@@ -18,13 +18,11 @@ import org.smartregister.giz.BaseRobolectricTest;
 import org.smartregister.giz.activity.AllClientsRegisterActivity;
 import org.smartregister.giz.activity.AncRegisterActivity;
 import org.smartregister.giz.activity.ChildRegisterActivity;
-import org.smartregister.giz.activity.GizAncProfileActivity;
 import org.smartregister.giz.activity.MaternityRegisterActivity;
 import org.smartregister.giz.activity.OpdRegisterActivity;
 import org.smartregister.giz.activity.PncRegisterActivity;
 import org.smartregister.giz.adapter.NavigationAdapter;
 import org.smartregister.giz.util.GizConstants;
-import org.smartregister.view.activity.BaseRegisterActivity;
 
 import java.util.ArrayList;
 
@@ -32,14 +30,12 @@ public class NavigationListenerTest extends BaseRobolectricTest {
 
     private Activity activity;
 
-    private NavigationAdapter navigationAdapter;
-
     private NavigationListener navigationListener;
 
     @Before
     public void setUp() {
-        navigationAdapter = new NavigationAdapter(new ArrayList<>(), activity);
-        activity = Mockito.spy(Robolectric.buildActivity(GizAncProfileActivity.class).create().get());
+        NavigationAdapter navigationAdapter = new NavigationAdapter(new ArrayList<>(), activity);
+        activity = Mockito.spy(Robolectric.buildActivity(OpdRegisterActivity.class).create().get());
         navigationListener = new NavigationListener(activity, navigationAdapter);
     }
 
@@ -50,27 +46,37 @@ public class NavigationListenerTest extends BaseRobolectricTest {
         //check for child register
         view.setTag(GizConstants.DrawerMenu.CHILD_CLIENTS);
         navigationListener.onClick(view);
-        isActivityStarted(ChildRegisterActivity.class);
+        Intent intent = new Intent(activity, ChildRegisterActivity.class);
+        Intent actual = Shadows.shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+        Assert.assertEquals(intent.getComponent(), actual.getComponent());
 
         //check for all clients
         view.setTag(GizConstants.DrawerMenu.ALL_CLIENTS);
         navigationListener.onClick(view);
-        isActivityStarted(AllClientsRegisterActivity.class);
+        intent = new Intent(activity, AllClientsRegisterActivity.class);
+        actual = Shadows.shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+        Assert.assertEquals(intent.getComponent(), actual.getComponent());
 
         //check for maternity clients
         view.setTag(GizConstants.DrawerMenu.MATERNITY_CLIENTS);
         navigationListener.onClick(view);
-        isActivityStarted(MaternityRegisterActivity.class);
+        intent = new Intent(activity, MaternityRegisterActivity.class);
+        actual = Shadows.shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+        Assert.assertEquals(intent.getComponent(), actual.getComponent());
 
         //check for pnc clients
         view.setTag(GizConstants.DrawerMenu.PNC_CLIENTS);
         navigationListener.onClick(view);
-        isActivityStarted(PncRegisterActivity.class);
+        intent = new Intent(activity, PncRegisterActivity.class);
+        actual = Shadows.shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+        Assert.assertEquals(intent.getComponent(), actual.getComponent());
 
         //check for opd clients
         view.setTag(GizConstants.DrawerMenu.OPD_CLIENTS);
         navigationListener.onClick(view);
-        isActivityStarted(OpdRegisterActivity.class);
+        intent = new Intent(activity, OpdRegisterActivity.class);
+        actual = Shadows.shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+        Assert.assertEquals(intent.getComponent(), actual.getComponent());
 
         //check for anc clients
         AncLibrary ancLibrary = Mockito.mock(AncLibrary.class);
@@ -81,13 +87,8 @@ public class NavigationListenerTest extends BaseRobolectricTest {
 
         view.setTag(GizConstants.DrawerMenu.ANC_CLIENTS);
         navigationListener.onClick(view);
-        isActivityStarted(AncRegisterActivity.class);
-    }
-
-
-    private void isActivityStarted(Class<? extends BaseRegisterActivity> s) {
-        Intent intent = new Intent(activity, s);
-        Intent actual = Shadows.shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+        intent = new Intent(activity, AncRegisterActivity.class);
+        actual = Shadows.shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
         Assert.assertEquals(intent.getComponent(), actual.getComponent());
     }
 }
