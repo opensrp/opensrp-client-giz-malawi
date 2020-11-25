61809e11d409dac28c3a0a7759f1745a378eea67
bennsimon <botieno@ona.io>
2020-11-25 12:22:54 +0300

    update report filter to last 4 months

--- opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/HIA2ReportsActivity.java
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/HIA2ReportsActivity.java
@@ -78,7 +78,7 @@ import static org.smartregister.util.JsonFormUtils.VALUE;
 public class HIA2ReportsActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE_GET_JSON = 3432;
-    public static final int MONTH_SUGGESTION_LIMIT = 10;
+    public static final int MONTH_SUGGESTION_LIMIT = 4;
     public static final String FORM_KEY_CONFIRM = "confirm";
     public static final DateFormat yyyyMMdd = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
 
