0de44fadbdc9d13e759f4ef01f1e5e55afd6a4f8
paulinembabu <pauline.mbabu@gmail.com>
2021-02-04 15:31:42 +0300

    Created on Dataload method and setupview methods

--- opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/ReportTypeListRegisterActivity.java
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/activity/ReportTypeListRegisterActivity.java
@@ -1,10 +1,7 @@
 package org.smartregister.giz.activity;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import org.smartregister.domain.FetchStatus;
 import org.smartregister.giz.R;
@@ -13,18 +10,16 @@ import org.smartregister.giz.model.ReportGroupingModel;
 import java.util.ArrayList;
 
 public class ReportTypeListRegisterActivity extends ReportRegisterActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        ListView listView = findViewById(R.id.lv_reportRegister_groupings);
-        TextView titleTv = findViewById(R.id.title);
-
+    public void setUpViews() {
+        super.setUpViews();
         if (titleTv != null) {
             titleTv.setText(R.string.reports_type);
         }
+    }
 
+    @Override
+    public void loadData() {
         final ArrayList<ReportGroupingModel.ReportGrouping> reportGroupings = getReportGroupings();
         listView.setAdapter(new ArrayAdapter<>(this, R.layout.report_types_list_item, reportGroupings));
         listView.setOnItemClickListener((parent, view, position, id) -> {
@@ -44,6 +39,12 @@ public class ReportTypeListRegisterActivity extends ReportRegisterActivity {
     }
 
     @Override
+    protected ArrayList<ReportGroupingModel.ReportGrouping> getReportGroupings() {
+        return (new ReportGroupingModel(this)).getReportListGroupings();
+    }
+
+
+    @Override
     public void onSyncInProgress(FetchStatus fetchStatus) {
         // Nothing to happen here
 
@@ -58,11 +59,5 @@ public class ReportTypeListRegisterActivity extends ReportRegisterActivity {
     @Override
     public void onSyncStart() {
         // Nothing to happen here
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        createDrawer();
     }
 }
