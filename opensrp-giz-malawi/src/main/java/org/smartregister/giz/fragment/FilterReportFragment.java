04aac5f7456dd0b0c9182de5d79739c4934df986
paulinembabu <pauline.mbabu@gmail.com>
2021-02-11 17:07:38 +0300

    Removes unused import

--- opensrp-giz-malawi/src/main/java/org/smartregister/giz/fragment/FilterReportFragment.java
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/fragment/FilterReportFragment.java
@@ -1,7 +1,6 @@
 package org.smartregister.giz.fragment;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
