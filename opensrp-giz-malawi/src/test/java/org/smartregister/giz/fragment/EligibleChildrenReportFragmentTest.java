79c36a1ea9ef665e5f8a43ca8c76725342df558c
paulinembabu <pauline.mbabu@gmail.com>
2021-05-18 15:09:36 +0300

    Fixed Failing test

--- opensrp-giz-malawi/src/test/java/org/smartregister/giz/fragment/EligibleChildrenReportFragmentTest.java
+++ opensrp-giz-malawi/src/test/java/org/smartregister/giz/fragment/EligibleChildrenReportFragmentTest.java
@@ -6,25 +6,40 @@ import android.widget.ProgressBar;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.powermock.reflect.Whitebox;
+import org.smartregister.view.ListContract;
 
 public class EligibleChildrenReportFragmentTest {
     private EligibleChildrenReportFragment fragment;
+    @Mock
+    private ListContract.Presenter presenter;
+
+    @Mock
+    private ListContract.View view;
+
+    @Mock
+    private ListContract.Model model;
+
+    @Mock
+    private ListContract.Interactor interactor;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         fragment = Mockito.mock(EligibleChildrenReportFragment.class, Mockito.CALLS_REAL_METHODS);
+        Mockito.when(presenter.getInteractor()).thenReturn(interactor);
+        Mockito.when(presenter.getModel()).thenReturn(model);
+        Mockito.when(presenter.getView()).thenReturn(view);
     }
 
     @Test
     public void testLoadPresenterAssignsPresenter() {
-        Assert.assertNull(Whitebox.getInternalState(fragment, "presenter"));
-
+        fragment.loadPresenter();
         Assert.assertNotNull(Whitebox.getInternalState(fragment, "presenter"));
-        Whitebox.getInternalState(fragment, "presenter");
+
     }
 
     @Test
