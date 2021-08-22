c9d69eea939633ffd80d39aa73e2c78d1e08f67b
paulinembabu <pauline.mbabu@gmail.com>
2021-08-22 11:11:03 +0300

    Removes unused imports

--- opensrp-giz-malawi/src/test/java/org/smartregister/giz/activity/ReportTypeListRegisterActivityTest.java
+++ opensrp-giz-malawi/src/test/java/org/smartregister/giz/activity/ReportTypeListRegisterActivityTest.java
@@ -10,7 +10,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnit;
