c405d493b29d4bcfeccca8baee14fa49f88a7779
vend <junaid.warsi@venturedive.com>
2023-02-02 11:20:53 +0500

    reformatted the code

--- opensrp-giz-malawi/src/main/java/org/smartregister/giz/recievers/GizReprocessSyncStatusReceiver.java
+++ opensrp-giz-malawi/src/main/java/org/smartregister/giz/recievers/GizReprocessSyncStatusReceiver.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import org.smartregister.giz.job.GIZClientReprocessJob;
+
 import timber.log.Timber;
+
 import static org.smartregister.receiver.SyncStatusBroadcastReceiver.EXTRA_COMPLETE_STATUS;
 
 public class GizReprocessSyncStatusReceiver extends BroadcastReceiver {
@@ -14,14 +16,13 @@ public class GizReprocessSyncStatusReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Bundle data = intent.getExtras();
         if (data != null) {
-            boolean isComplete = data.getBoolean(EXTRA_COMPLETE_STATUS,false);
-            if(isComplete)
-            {
+            boolean isComplete = data.getBoolean(EXTRA_COMPLETE_STATUS, false);
+            if (isComplete) {
                 GIZClientReprocessJob.scheduleJobImmediately(GIZClientReprocessJob.TAG);
                 Timber.d("fetch completed");
             }
         }
 
-       Timber.d("broadcast Recieved");
+        Timber.d("broadcast Recieved");
     }
 }
